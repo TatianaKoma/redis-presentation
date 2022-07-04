0ba4772c1b23cb7dab99c9ebc1a419b9a3ec6b90
Tatiana Komarova <tatiana78977ua@gmail.com>
2022-07-04 12:59:42 +0300

    first commit

---
+++ src/main/java/com/example/redispresentation/RedisPresentationApplication.java
@@ -0,0 +1,13 @@
+package com.example.redispresentation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisPresentationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisPresentationApplication.class, args);
+    }
+
+}
